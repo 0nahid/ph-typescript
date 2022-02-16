2fa4ba697d2844c8ad8221240b3a5b56d7e3a207
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-17 02:43:53 +0600

    added typescript array

---
+++ index.js
@@ -0,0 +1,5 @@
+// Array <T>
+var arr = [1, 2, 3];
+var arr2 = [1, 2, 3];
+var arr3 = ["a", "b", "c"];
+var arr4 = ["a", "b", "c"];
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Array <T>
+const arr: number[] = [1, 2, 3];
+const arr2: Array<number> = [1, 2, 3];
+const arr3: string[] = ["a", "b", "c"];
+const arr4: Array<string> = ["a", "b", "c"];
+
