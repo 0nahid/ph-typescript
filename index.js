d0a8b29f2bbadd4100e75fc4d669eae6b2c8c0b0
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-02-17 02:44:17 +0600

    added type alias

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ var arr = [1, 2, 3];
 var arr2 = [1, 2, 3];
 var arr3 = ["a", "b", "c"];
 var arr4 = ["a", "b", "c"];
+var id = true;
--- index.ts
+++ index.ts
@@ -4,3 +4,7 @@ const arr2: Array<number> = [1, 2, 3];
 const arr3: string[] = ["a", "b", "c"];
 const arr4: Array<string> = ["a", "b", "c"];
 
+// Type alias
+type numOrStrOrBol = number | string | boolean;
+const id: numOrStrOrBol = true;
+
